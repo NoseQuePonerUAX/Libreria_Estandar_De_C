3322d1dfca6e2cc4c174221718f3e7d4a34c06d0
NoseQuePonerUAX <joserafe@myuax.com>
2023-12-19 01:26:03 +0100

    funcion strlen

--- cadenas.c
+++ cadenas.c
@@ -3,13 +3,17 @@
 
 int main() {
 
-    char str1[] = "Hola";
-    char str2[] = "Mundo";
+    char str1[] = "Pelota";
+    char str2[] = "Bellota";
     int comparison = strcmp(str1, str2);
     if (comparison == 0) {
         printf("Las cadenas son iguales\n");
     } else {
         printf("Las cadenas son diferentes.\n");
     }
+
+    char str[] = "Que pasa amigos del bien";
+    int length = strlen(str);
+    printf("La longitud de '%s' es: %d\n", str, length);
     return 0;
 }
