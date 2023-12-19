12d361119ecb97cf0f0a8b016fffbfe8bb3262d2
NoseQuePonerUAX <joserafe@myuax.com>
2023-12-19 01:37:20 +0100

    Archivos b

--- archivos.c
+++ archivos.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
 int main() {
+
     FILE *file = fopen("file.txt", "r");
     if (file == NULL) {
         printf("El archivo no se logra abrir.\n");
         return 1;
     }
+
     fclose(file);
+
+    FILE *file2 = fopen("file2.txt", "w");
+    if (file2 == NULL) {
+        printf("El archivo no se logra abrir para escritura.\n");
+        return 1;
+    }
+    char buffer[50];
+    fscanf(file2, "%s", buffer);
+    printf("Leido del archivo: %s\n", buffer);
+
+    fclose(file2);
+
     return 0;
 }
