0800e5287e5cc97055327183396d104855081573
NoseQuePonerUAX <joserafe@myuax.com>
2023-12-19 01:44:05 +0100

    Ejemplo 1 de 3 digitos

--- main.c
+++ main.c
@@ -9,8 +9,12 @@ int main() {
     printf("El valor absoluto de %d es %d\n", num, abs_num);
 
     srand(time(0));
-    int random_number = rand();
+    int random_number = rand() ;
     printf("Numero aleatorio: %d\n", random_number);
+
+    srand(time(0));
+    int random_number2 = rand() % 100 + 10;
+    printf("Numero aleatorio entre 10 y 100: %d\n", random_number2);
 
     return 0;
 }
