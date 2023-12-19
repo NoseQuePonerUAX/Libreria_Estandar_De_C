1be594ad7c3a7d0613978abc4ad143bc1b55ea57
NoseQuePonerUAX <joserafe@myuax.com>
2023-12-19 01:28:39 +0100

    Ejemplo strcmp

--- cadenas.c
+++ cadenas.c
@@ -12,6 +12,15 @@ int main() {
         printf("Las cadenas son diferentes.\n");
     }
 
+    char str3[] = "Juan";
+    char str4[] = "Juan";
+    int comparison2 = strcmp(str3, str4);
+    if (comparison2 == 0) {
+        printf("Las cadenas son iguales\n");
+    } else {
+        printf("Las cadenas son diferentes.\n");
+    }
+
     char str[] = "Que pasa amigos del bien";
     int length = strlen(str);
     printf("La longitud de '%s' es: %d\n", str, length);
