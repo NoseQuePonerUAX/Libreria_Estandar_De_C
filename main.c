ea8af84a77e63ddbdae94beed1cc5a3593fe1e9e
NoseQuePonerUAX <joserafe@myuax.com>
2023-12-19 01:11:46 +0100

    Funciones comunes2

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 int main() {
+
     int num = -10;
     int abs_num = abs(num);
     printf("El valor absoluto de %d es %d\n", num, abs_num);
+
+    srand(time(0));
+    int random_number = rand();
+    printf("Numero aleatorio: %d\n", random_number);
+    
     return 0;
 }
